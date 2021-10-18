93f6ed306392ced437d473e04914fd93e6f7a3ec
emanuelmilotic56 <58472080+emanuelmilotic56@users.noreply.github.com>
2021-10-18 09:12:31 +0200

    Add files via upload

---
+++ ZadatakIspit1-EM/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatakIspit1_EM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sport sport1 = new Sport("Nogomet", true, 11);
+            Sport sport2 = new Sport("Vaterpolo", true, 7);
+            Sport sport3 = new Sport("Curling", false, 5);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ZadatakIspit1-EM/Sport.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatakIspit1_EM
+{
+    class Sport
+    {
+        private string Naziv;
+        private bool IgraLiSeSportom;
+        private byte BrojIgraca;
+
+        public Sport(string s)
+        {
+            Naziv = s;
+        }
+        public Sport(string s, bool b)
+        {
+            Naziv = s;
+            IgraLiSeSportom = b;
+        }
+        public Sport(string s, bool b, byte n)
+        {
+            Naziv = s;
+            IgraLiSeSportom = b;
+            BrojIgraca = n;
+        }
+
+    }
+}
